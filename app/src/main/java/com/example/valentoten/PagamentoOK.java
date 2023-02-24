0951f1edffc674a411e1304c8ebdacaebdbff2a3
José Neto <ribeirojoseph44@gmail.com>
2023-02-24 12:03:40 -0300

    Finalizando as telas

---
+++ app/src/main/java/com/example/valentoten/PagamentoOK.java
@@ -0,0 +1,14 @@
+package com.example.valentoten;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PagamentoOK extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagamento_ok);
+    }
+}
